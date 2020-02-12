adfa81899dee39f6ec36b3cb4abbbaa04d944255
Jhonatan Sandoval <jsandovalbazan@gmail.com>
2020-02-12 10:30:19 -0500

    connection to mongodb

--- src/app.js
+++ src/app.js
@@ -8,6 +8,16 @@ const app = express();
 app.use(cors())
 app.set('port', port);
 
+// connection to mongo
+require('mongoose')
+    .connect("mongodb://localhost/seguridad_ciudadana", {
+        useCreateIndex: true,
+        useNewUrlParser: true
+    })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB Connection error', err))
+
+
 // config routes
 const userRoute = require('./routes/userRoute')
 app.use('/users', userRoute)
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 module.exports = {
-    SERVER_ERROR_MESSAGE = 'There was an error processing this request'
+    SERVER_ERROR_MESSAGE: 'There was an error processing this request'
 }
